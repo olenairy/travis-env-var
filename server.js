926536318f05ca516372178efe9fb9b15f41142a
Olena Chudnovets <olenairy@gmail.com>
2021-01-14 10:44:18 -0400

    Change server.js file2 commit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -6,7 +7,7 @@ function sleep(ms) {
 async function main() {
   while(true) {
 
-    const favoriteFood = process.env.FAVORITE_FOOD;
+    
     console.log('My favorite food is ${favoriteFood}');
     
     await sleep(5000);
