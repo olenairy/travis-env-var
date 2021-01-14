5de4a6862f9f8e3d8667ede4f11a9d1a7995960f
Olena Chudnovets <olenairy@gmail.com>
2021-01-14 10:26:34 -0400

    Change server.js file commit

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log("Microservices rock!");
+
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log('My favorite food is ${favoriteFood}');
+    
     await sleep(5000);
   }
 }
